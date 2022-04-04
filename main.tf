470a5f16c455aa30f93581135841b4fe4ee27ed1
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-04-04 19:33:20 +0100

    created docker-install.sh

--- main.tf
+++ main.tf
@@ -282,16 +282,14 @@ resource "aws_instance" "devops106_terraform_dungureanu_webserver_app_tf" {
         destination = "/home/ubuntu/database.config"
     }
 
+    provisioner "file" {
+        source = "./init-scripts/docker-install.sh"
+        destination = "/home/ubuntu/docker-install.sh"
+    }
+
     provisioner "remote-exec" {
         inline = [
-            "sudo apt-get remove -y docker docker-engine docker.io containerd runc",
-            "sudo apt-get update",
-            "sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release",
-            "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-            "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-            "sudo apt-get update",
-            "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
-            "sudo usermod -a -G docker ubuntu"
+            "bash /home/ubuntu/docker-install.sh"
         ]
     }
 
