66336c988667f198beacca33053856e3bec027a7
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-04-07 14:13:19 +0100

    added db ip and proxy ip output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "webserver_ip_addresses_output" {
-  value = aws_instance.devops106_dungureanu_terraform_webserver_app_tf[*].public_ip
+  value = aws_instance.devops106_terraform_daniel_webserver_app_tf[*].public_ip
+}
+
+output "mongoDB_server_ip_address" {
+  value = aws_instance.devops106_terraform_daniel_webserver_db_tf.public_ip
+}
+
+output "proxy_server_ip_address" {
+  value = aws_instance.devops106_terraform_daniel_proxy_server_tf.public_ip
 }
