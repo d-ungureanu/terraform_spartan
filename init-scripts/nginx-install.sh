d4ebc2d1fb31fd823ea90a295c8f5ec01c5591d6
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-04-11 10:16:19 +0100

    updated nginx install for config file generation

--- init-scripts/nginx-install.sh
+++ init-scripts/nginx-install.sh
@@ -3,8 +3,26 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo systemctl start nginx
-sudo systemctl enable nginx
-
 sudo mv /etc/nginx/sites-available/default .
 sudo rm /etc/nginx/sites-enabled/default
-sudo echo test line 1 > test.sh
+sudo tee -a /etc/nginx/sites-available/spartan_web << END
+upstream spartan_servers {
+        server web1.dungureanu.devops106:5000;
+        server web2.dungureanu.devops106:5000;
+        server web3.dungureanu.devops106:5000;
+        server web4.dungureanu.devops106:5000;
+}
+
+server {
+        listen 80;
+        server_name _;
+        location / {
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_pass http://spartan_servers;
+        }
+}
+END
+sudo ln -s /etc/nginx/sites-available/spartan_web /etc/nginx/sites-enabled/
+sudo systemctl restart nginx
+sudo systemctl enable nginx
