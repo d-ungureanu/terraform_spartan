15d16593cac1ccc64f364e036bb6266905ccbb61
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-04-13 14:38:37 +0100

    added incoming 443 rules to nacl and security group removed aws load balancer and using nginx

--- main.tf
+++ main.tf
@@ -114,6 +114,15 @@ resource "aws_network_acl" "devops106_terraform_daniel_nacl_app_public_tf" {
   }
 
   ingress {
+    rule_no    = 400
+    from_port  = 443
+    to_port    = 443
+    cidr_block = "0.0.0.0/0"
+    protocol   = "tcp"
+    action     = "allow"
+  }
+
+  ingress {
     rule_no    = 10000
     from_port  = 1024
     to_port    = 65535
@@ -238,6 +247,13 @@ resource "aws_security_group" "devops106_terraform_daniel_sg_app_webserver_tf" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -422,49 +438,49 @@ resource "aws_route53_record" "devops106_terraform_daniel_dns_db_tf" {
   records = [aws_instance.devops106_terraform_daniel_webserver_db_tf.public_ip]
 }
 
-resource "aws_lb" "devops106_terraform_daniel_lb_tf" {
-  name               = "devops106-terraform-daniel-lb"
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = [
-    aws_subnet.devops106_terraform_daniel_subnet_app_webserver_tf.id,
-    aws_subnet.devops106_terraform_daniel_subnet_app_webserver2_tf.id
-  ]
-  security_groups = [
-    aws_security_group.devops106_terraform_daniel_sg_app_webserver_tf.id
-  ]
-
-  tags = {
-    Name = "devops106_terraform_daniel_lb"
-  }
-}
-
-resource "aws_alb_target_group" "devops106_terraform_daniel_tg_tf" {
-  name        = "devops106-terraform-daniel-tg"
-  port        = 5000
-  target_type = "instance"
-  protocol    = "HTTP"
-  vpc_id      = local.vpc_id_var
-}
-
-resource "aws_alb_target_group_attachment" "devops106_terraform_daniel_tg_attach_tf" {
-  target_group_arn = aws_alb_target_group.devops106_terraform_daniel_tg_tf.arn
-  count            = length(aws_instance.devops106_terraform_daniel_webserver_app_tf)
-  target_id        = aws_instance.devops106_terraform_daniel_webserver_app_tf[count.index].id
-}
-
-resource "aws_alb_target_group_attachment" "devops106_terraform_daniel_tg_attach2_tf" {
-  target_group_arn = aws_alb_target_group.devops106_terraform_daniel_tg_tf.arn
-  count            = length(aws_instance.devops106_terraform_daniel_webserver2_app_tf)
-  target_id        = aws_instance.devops106_terraform_daniel_webserver2_app_tf[count.index].id
-}
-
-resource "aws_lb_listener" "devops106_terraform_daniel_lb_listener_tf" {
-  load_balancer_arn = aws_lb.devops106_terraform_daniel_lb_tf.arn
-  port = 80
-  protocol = "HTTP"
-  default_action {
-    type = "forward"
-    target_group_arn = aws_alb_target_group.devops106_terraform_daniel_tg_tf.arn
-  }
-}
+#resource "aws_lb" "devops106_terraform_daniel_lb_tf" {
+#  name               = "devops106-terraform-daniel-lb"
+#  internal           = false
+#  load_balancer_type = "application"
+#  subnets            = [
+#    aws_subnet.devops106_terraform_daniel_subnet_app_webserver_tf.id,
+#    aws_subnet.devops106_terraform_daniel_subnet_app_webserver2_tf.id
+#  ]
+#  security_groups = [
+#    aws_security_group.devops106_terraform_daniel_sg_app_webserver_tf.id
+#  ]
+#
+#  tags = {
+#    Name = "devops106_terraform_daniel_lb"
+#  }
+#}
+#
+#resource "aws_alb_target_group" "devops106_terraform_daniel_tg_tf" {
+#  name        = "devops106-terraform-daniel-tg"
+#  port        = 5000
+#  target_type = "instance"
+#  protocol    = "HTTP"
+#  vpc_id      = local.vpc_id_var
+#}
+#
+#resource "aws_alb_target_group_attachment" "devops106_terraform_daniel_tg_attach_tf" {
+#  target_group_arn = aws_alb_target_group.devops106_terraform_daniel_tg_tf.arn
+#  count            = length(aws_instance.devops106_terraform_daniel_webserver_app_tf)
+#  target_id        = aws_instance.devops106_terraform_daniel_webserver_app_tf[count.index].id
+#}
+#
+#resource "aws_alb_target_group_attachment" "devops106_terraform_daniel_tg_attach2_tf" {
+#  target_group_arn = aws_alb_target_group.devops106_terraform_daniel_tg_tf.arn
+#  count            = length(aws_instance.devops106_terraform_daniel_webserver2_app_tf)
+#  target_id        = aws_instance.devops106_terraform_daniel_webserver2_app_tf[count.index].id
+#}
+#
+#resource "aws_lb_listener" "devops106_terraform_daniel_lb_listener_tf" {
+#  load_balancer_arn = aws_lb.devops106_terraform_daniel_lb_tf.arn
+#  port = 80
+#  protocol = "HTTP"
+#  default_action {
+#    type = "forward"
+#    target_group_arn = aws_alb_target_group.devops106_terraform_daniel_tg_tf.arn
+#  }
+#}
